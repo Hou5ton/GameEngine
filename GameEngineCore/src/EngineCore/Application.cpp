eddf78e15c5146011d817084b7b01aa507f61e7a
Eugene <eugene.skrobot@gmail.com>
2021-12-21 00:43:29 +0300

    connection glad

--- GameEngineCore/src/EngineCore/Application.cpp
+++ GameEngineCore/src/EngineCore/Application.cpp
@@ -1,6 +1,7 @@
 #include "EngineCore/Application.hpp"
 #include "EngineCore/Log.hpp"
 
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
@@ -46,11 +47,20 @@ namespace SimpleEngine
         /* Make the window's context current */
         glfwMakeContextCurrent(window);
 
+
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        {
+            LOG_CRITICAL("Failed to initialize GLAD");
+            return -1;
+        }
+
+        glClearColor(1, 0, 0, 0);
+
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
-            //glClear(GL_COLOR_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT);
 
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
